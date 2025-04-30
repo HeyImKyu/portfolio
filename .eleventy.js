438d5f01575686a61c08ab7e9bff34f88d9346b1
Kyu <heyimkyu@mailbox.org>
2025-04-30 22:24:14 +0200

    nunjucks-date added

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,29 @@
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("src/assets");
-  eleventyConfig.addPassthroughCopy("src/js");
-  eleventyConfig.addPassthroughCopy("src/css");
+  const passthroughs = [
+    "src/assets",
+    "src/js",
+    "src/css"
+  ]
+  
+  const config = {
+    "testglobal": "meowmeow",
+  };
+
+  // ------- adding the stuff
+  
+  for (const item in config) {
+    eleventyConfig.addGlobalData(item, config[item]);
+  }
+
+  for (const item of passthroughs) {
+    eleventyConfig.addPassthroughCopy(item);
+  }
+
+  var nunjucksDate = require("nunjucks-date");
+  // http://momentjs.com/docs/#/displaying/format/
+  nunjucksDate.setDefaultFormat("DD.MM.YYYY");
+  eleventyConfig.addFilter("date", nunjucksDate);
+
   return {
     dir: {
       input: "src",   // read source files from "src"
