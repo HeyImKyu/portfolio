1964513e7d735bbd3dee957c8beceb969f81d478
Kyu <heyimkyu@mailbox.org>
2025-05-01 00:05:59 +0200

    reorder darktoggle won the fight against fouc :3

---
+++ src/js/darktoggle.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleSwitch = document.querySelector(
+    '.darkmode-label input[type="checkbox"]'
+    );
+    const container = document.getElementById('darkmode-container');
+
+    toggleSwitch.checked = localStorage.getItem('darkmode') === 'true';
+
+    function switchTheme(dark) {
+        if (dark) {
+            document.documentElement.classList.add("dark");
+            container.classList.add("dark");
+        } else {
+            document.documentElement.classList.remove("dark");
+            container.classList.remove("dark");
+        }
+
+        localStorage.setItem('darkmode', dark);
+    }
+
+    function switchThemeListener(e) {
+        switchTheme(e.target.checked)
+    }
+
+    switchTheme(toggleSwitch.checked);
+    toggleSwitch.addEventListener("change", switchThemeListener, false);
+});
+
